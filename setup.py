1bc95965729dbb7e5e1e16abc94002a478932060
Victor Porton <porton@narod.ru>
2018-01-31 09:10:33 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='xml-boiler',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/vporton/xml-boiler',
+    license='AGPLv3',
+    author='Victor Porton',
+    author_email='porton@narod.ru',
+    description='Automatically transform between XML namespaces￼'
+)
