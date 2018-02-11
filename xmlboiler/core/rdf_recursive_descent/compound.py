780a42a00a8ef8f28daf583402539e265635a953
Victor Porton <porton@narod.ru>
2018-02-11 19:41:30 +0200

    missing license headers

--- xmlboiler/core/rdf_recursive_descent/compound.py
+++ xmlboiler/core/rdf_recursive_descent/compound.py
@@ -16,21 +16,3 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from rdflib import URIRef
-
-from .base import *
-
-
-class EnumParser(NodeParserWithError):
-    def __init__(self, map, on_error):
-        super(NodeParserWithError, self).__init__(on_error)
-        self.map = map
-
-    def parse(self, parse_context, graph, node):
-        if node not in URIRef:
-            self.throw(lambda: parse_context.translate("Node %s should be an IRI.") % str(node))
-        try:
-            value = self.map[node]
-        except KeyError:
-            self.throw(lambda: parse_context.translate("The IRI %s is unknown.") % str(node))
-        return value
