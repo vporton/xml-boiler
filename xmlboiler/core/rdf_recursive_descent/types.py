1b5ce4433a24a3b299844ee1a71f2ce44cbcb575
Victor Porton <porton@narod.ru>
2018-02-12 09:29:55 +0200

    finsihed RDF recursive descent

---
+++ xmlboiler/core/rdf_recursive_descent/types.py
@@ -0,0 +1,48 @@
+#  Copyright (c) 2018 Victor Porton,
+#  XML Boiler - http://freesoft.portonvictor.org
+#
+#  This file is part of XML Boiler.
+#
+#  XML Boiler is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from rdflib import RDF, URIRef
+
+from xmlboiler.core.rdf_recursive_descent.base import ParseException
+
+
+def check_node_class(relation, parse_context, graph, node, klass, on_error):
+    for i in graph.objects(node, RDF.type):
+        if i in URIRef and relation.is_connected(i, klass):
+            return
+    str = lambda: parse_context.translate("Node {node} must be of class {klass}.").format(node=node, klass=klass)
+    parse_context.throw(on_error, str)
+
+
+class ClassForestParser(object):
+    def __init__(self, node_parser, klass, subclasses):
+        self.node_parser = node_parser
+        self.klass = klass
+        self.subclasses = subclasses
+
+    def parse(self, parse_context, graph):
+        result = []
+        # TODO: Not the fastest algorithm
+        # TODO: It does a wrong thing if one node has multiple "type" predicates
+        for node, nodeClass in result[:RDF.type]:
+            if nodeClass in URIRef and self.subclasses.is_connected(nodeClass, self.klass):
+                try:
+                    result.append(self.node_parser.parse(parse_context, graph, node))
+                except ParseException:
+                    pass
+        return result
