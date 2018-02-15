153f39fc0aaa94c4c518a14ce339612e06afb993
Victor Porton <porton@narod.ru>
2018-02-15 12:31:04 +0200

    thread-safe subparsers

--- xmlboiler/core/rdf_format/asset_parser/script.py
+++ xmlboiler/core/rdf_format/asset_parser/script.py
@@ -16,11 +16,13 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from dependency_injector.providers import ThreadLocalSingleton
+from dependency_injector.containers import DeclarativeContainer
 from xmlboiler.core.rdf_format.base import MAIN_NAMESPACE
 from xmlboiler.core.rdf_recursive_descent.base import NodeParser
 from xmlboiler.core.rdf_recursive_descent.compound import Choice
 from xmlboiler.core.rdf_recursive_descent.enum import EnumParser
-from xmlboiler.core.rdf_format.asset import AssetInfo, TransformerKindEnum
+from xmlboiler.core.rdf_format.asset import AssetInfo, TransformerKindEnum, ValidatorKindEnum
 
 
 class ScriptInfoParser(Choice):
@@ -37,20 +39,19 @@ class BaseScriptInfoParser(NodeParser):
     def __init__(self, script_kind):
         self.script_kind = script_kind
 
-    # FIXME: Race conditions when multithreading!
-    _transformer_kind_parser = None
-    _validator_kind_parser   = None
-
     @staticmethod
-    def transformer_kind_parser():
-        if BaseScriptInfoParser._transformer_kind_parser is not None:
-            return BaseScriptInfoParser._transformer_kind_parser
+    def create_transformer_kind_parser():
         map = {MAIN_NAMESPACE + "entire": TransformerKindEnum.ENTIRE,
                MAIN_NAMESPACE + "sequential": TransformerKindEnum.SEQUENTIAL,
                MAIN_NAMESPACE + "upDown": TransformerKindEnum.UP_DOWN,
                MAIN_NAMESPACE + "downUp": TransformerKindEnum.DOWN_UP}
-        BaseScriptInfoParser._transformer_kind_parser = EnumParser(map)
-        return BaseScriptInfoParser._transformer_kind_parser
+        return EnumParser(map)
+
+    @staticmethod
+    def create_validator_kind_parser():
+        map = {MAIN_NAMESPACE + "entire": ValidatorKindEnum.ENTIRE,
+               MAIN_NAMESPACE + "parts": ValidatorKindEnum.PARTS}
+        return EnumParser(map)
 
     # TODO
 
@@ -61,3 +62,8 @@ class CommandScriptInfoParser(NodeParser):
 # TODO
 class WebServiceScriptInfoParser(NodeParser):
     pass
+
+class Providers(DeclarativeContainer):
+    # What is more efficient: thread-local or thread-safe?
+    transformer_kind_parser = ThreadLocalSingleton(BaseScriptInfoParser.create_transformer_kind_parser)
+    validator_kind_parser   = ThreadLocalSingleton(BaseScriptInfoParser.create_validator_kind_parser  )
