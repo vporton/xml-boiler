0a57b0ddaa395b5bde41819d06e37d646e1d319a
Victor Porton <porton@narod.ru>
2018-02-17 15:04:54 +0200

    determining Debian package version

---
+++ xmlboiler/core/packages/debian.py
@@ -0,0 +1,28 @@
+#  Copyright (c) 2018 Victor Porton,
+#  XML Boiler - http://freesoft.portonvictor.org
+#
+#  This file is part of XML Boiler.
+#
+#  XML Boiler is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import deb_pkg_tools
+
+from .base import BasePackageManaging
+
+
+class DebianPackageManaging(BasePackageManaging):
+    def determine_package_version(cls):
+        return deb_pkg_tools.utils.find_installed_version(package_name)
+
+    # See https://deb-pkg-tools.readthedocs.io/en/latest/ about version comparison.
