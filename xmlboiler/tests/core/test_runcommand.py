aca51975aa80af94a37aee22d40340ba09a42a06
Victor Porton <porton@narod.ru>
2018-02-19 22:05:28 +0200

    more tests, now doesn't pass

--- xmlboiler/tests/core/test_runcommand.py
+++ xmlboiler/tests/core/test_runcommand.py
@@ -3,8 +3,16 @@ import unittest
 from xmlboiler.core.os_command.regular import *
 
 class TestRunCommand(unittest.TestCase):
-    def test_cat_short(self):
-        input = b"qwe"
-        code, output = RegularCommandRunner.run_pipe(['cat'], input, timeout=None, timeout2=None)
+    def setUp(self):
+        self.long = bytes(map(lambda i: i%3, range(1000000)))
+
+    def do_test_ok(self, command, input):
+        code, output = RegularCommandRunner.run_pipe(command, input, timeout=None, timeout2=None)
         self.assertEqual(code, 0)
         self.assertEqual(input, output)
+
+    def test_cat_short(self):
+        self.do_test_ok(['cat'], b"qwe")
+
+    def test_cat_long(self):
+        self.do_test_ok(['cat'], self.long)
