c81f00cee141cfc20aec81e22bf474ef4fa6a7af
Victor Porton <porton@narod.ru>
2018-02-19 20:35:04 +0200

    spacing

--- xmlboiler/core/os_command/base.py
+++ xmlboiler/core/os_command/base.py
@@ -16,8 +16,10 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+
 class Timeout(Exception):
     pass
+
 
 class BaseCommandRunner(object):
     @classmethod
