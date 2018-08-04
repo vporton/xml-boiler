d1f97b2eee6381b19267b227842740c391aa1e88
Victor Porton <porton@narod.ru>
2018-08-04 17:02:44 +0300

    bug fix

--- xmlboiler/core/alg/auto_transform.py
+++ xmlboiler/core/alg/auto_transform.py
@@ -34,8 +34,8 @@ class AutomaticTranformation(object):
         parents = [self.state.dom.documentElement]
         while parents:
             v = parents.pop()
-            if v.namespaceURI is not None:
-                all_namespaces.append(v.namespaceURI)
+            # if v.namespaceURI is not None:  # even if it is None add it to the set
+            all_namespaces.add(v.namespaceURI)
             for w in v.childNodes:
                 parents.append(w)
 
