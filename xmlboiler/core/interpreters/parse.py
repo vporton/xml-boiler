61362059361f7d181edaf53b679dffd919984ae8
Victor Porton <porton@narod.ru>
2018-08-06 18:14:10 +0300

    bug fix

--- xmlboiler/core/interpreters/parse.py
+++ xmlboiler/core/interpreters/parse.py
@@ -132,4 +132,4 @@ class Interpeters(object):
 # TODO: Use proper dependency injection instead of the singleton
 interpreters = providers.ThreadLocalSingleton(Interpeters,
                                               execution_context=execution_context_builders.Contexts.execution_context,
-                                              graph=Global.load_rdf('interpreters.ttl'))
+                                              graph=Global.load_rdf('core/data/interpreters.ttl'))
