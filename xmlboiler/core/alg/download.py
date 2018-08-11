c7b1637470c2df08304ae7a4beee70763622c7ac
Victor Porton <porton@narod.ru>
2018-08-11 20:33:03 +0300

    bug fix

--- xmlboiler/core/alg/download.py
+++ xmlboiler/core/alg/download.py
@@ -105,8 +105,8 @@ class DepthFirstDownloader(BaseDownloadAlgorithm):
             self.state.add_asset(asset_info)
         yield
         for ns2 in _enumerate_child_namespaces_without_priority(self.state, ns):
-            # if ns2 not in self.state.assets: # checked above
-            yield from self.depth_first_download(ns2, downloaders)  # recursion
+            if ns2 not in self.state.assets:
+                yield from self.depth_first_download(ns2, downloaders)  # recursion
 
     # Every yield produces a list of assets (not individual assets),
     # because in our_depth_first_based_download() we need to discard multiple assets.
