d0446d8f62f4a6f598d953cb5df553fe173c7090
Victor Porton <porton@narod.ru>
2018-08-12 17:30:27 +0300

    removed an outdated FIXME comment

--- xmlboiler/core/alg/next_script_base.py
+++ xmlboiler/core/alg/next_script_base.py
@@ -117,7 +117,6 @@ class ScriptsIteratorBase(ABC):
 
         return result
 
-    # FIXME: This does not support universal scripts
     # Almost duplicate code with all_childs_in_target_hash()
     def first_childs_in_target(self):
         # use depth-first search
