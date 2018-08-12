dddf6cfb4a0bcda78abe8a22bbd18efd312d5876
Victor Porton <porton@narod.ru>
2018-08-12 17:15:36 +0300

    bug fix

--- xmlboiler/core/rdf_format/asset_parser/namespace.py
+++ xmlboiler/core/rdf_format/asset_parser/namespace.py
@@ -34,8 +34,10 @@ class NSParser(NodeParser):
         klass = URIRef(MAIN_NAMESPACE + "Namespace")
         check_node_class(self.subclasses, parse_context, graph, node, klass, ErrorHandler.IGNORE)
 
-        script_node_parser = ScriptInfoParser(None, self.subclasses, ScriptKindEnum.VALIDATOR)  # FIXME: passing None as transformer arg is an error
-        script_parser = OneOrMorePredicate(URIRef(MAIN_NAMESPACE + "validator"), script_node_parser, ErrorHandler.WARNING)
-        validators = script_parser.parse(parse_context, graph, node)
+        result = Namespace(resource=node)
 
-        return Namespace(resource=node, validators=validators)
+        script_node_parser = ScriptInfoParser(result, self.subclasses, ScriptKindEnum.VALIDATOR)
+        script_parser = OneOrMorePredicate(URIRef(MAIN_NAMESPACE + "validator"), script_node_parser, ErrorHandler.WARNING)
+        result.validators = script_parser.parse(parse_context, graph, node)
+
+        return result
