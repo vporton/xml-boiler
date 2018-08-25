737cec93f7c73432023926050d65dba09d06c726
Victor Porton <porton@narod.ru>
2018-08-25 03:21:15 +0300

    setup.py obtains file list from Git

--- setup.py
+++ setup.py
@@ -11,16 +11,19 @@ class MyBuild(DistutilsBuild):
 
 setup(
     name='xml-boiler',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/vporton/xml-boiler',
     license='AGPLv3',
     author='Victor Porton',
     author_email='porton@narod.ru',
     description='Automatically transform between XML namespaces',
 
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     packages=find_packages(),
-    package_data={'': ['*.xml', '**/*.xml', '*.ttl', '**/*.ttl', '*.net', '**/*.net', 'data/assets/*', 'data/scripts/*.xslt',
-                       'tests/core/data/xml/*.xml', 'doc/*.html']},
+    # package_data={'': ['**/*.xml', '**/*.ttl', '**/*.net', 'data/assets/*', 'data/scripts/*.xslt',
+    #                    'xmlboiler/doc/*.html', 'xmlboiler/doc/*.css']},
+    include_package_data=True,
     scripts=['bin/boiler'],
     data_files = [
         ('/etc/xmlboiler', ['etc/config-cli.ttl'])
