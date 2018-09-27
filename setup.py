a4c9c8cd2f11ad5dfb8a4eab874f24b2f9decacf
Victor Porton <porton@narod.ru>
2018-09-27 17:58:38 +0300

    new version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ class MyBuild(DistutilsBuild):
 
 setup(
     name='xml-boiler',
-    version='0.2.6',
+    version='0.2.7',
     url='https://github.com/vporton/xml-boiler',
     license='AGPLv3',
     author='Victor Porton',
