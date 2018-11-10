e273514fa7cf45e0861bc57610835fe76ec131e4
Victor Porton <porton@narod.ru>
2018-11-10 22:57:59 +0200

    removed a TODO

--- xmlboiler/command_line.py
+++ xmlboiler/command_line.py
@@ -71,7 +71,7 @@ def main(argv):
     chain_parser.add_argument('-o', '--output', nargs=1, help='output file (defaults to stdout)')
     chain_parser.add_argument('-t', '--target', help='target namespace(s)', action='append', metavar='NAMESPACE')
     chain_parser.add_argument('-s', '--next-script', help='"next script" algorithm ("precedence" is not supported)',
-                              choices=['precedence', 'doc'], default='doc')  # TODO: subject to change notation
+                              choices=['precedence', 'doc'], default='doc')
     chain_parser.add_argument('-n', '--not-in-target', help='what if a result is not in target NS',
                               choices=['ignore', 'remove', 'error'])
     chain_parser.add_argument('-u', '--universal-precedence', help='universal precedence', metavar='URL')
