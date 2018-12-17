b7f1b54e9cda0b6c40fd35511a78827a7ecfd042
Victor Porton <porton@narod.ru>
2018-12-17 03:31:45 +0200

    code moved to correct place

--- xmlboiler/core/alg/auto_transform.py
+++ xmlboiler/core/alg/auto_transform.py
@@ -19,8 +19,40 @@
 from dependency_injector import containers, providers
 from rdflib import URIRef
 
-from xmlboiler.core.alg.common import RealNextScript
+from xmlboiler.core.alg.common import log_script_execution
 from xmlboiler.core.util.xml import myXMLParseString
+
+# it skips scripts for which there is no interpreter
+from xmlboiler.core.scripts.xml import Providers
+
+
+class RealNextScript(object):
+    def __init__(self, state, interpreters, xml_run_command=Providers.xml_run_command):
+        self.state = state
+        self.interpreters = interpreters
+        self.xml_run_command = xml_run_command
+
+    def step(self):
+        while True:
+            script = next(self.state.next_script)['script']
+            node = self.interpreters.find_interpreter(script.more.language, script.more.min_version,
+                                                      script.more.max_version)
+            if node is None:
+                self.state.failed_scripts.add(script)
+            else:
+                log_script_execution(self.state, script)
+                self.state.executed_scripts.add(script)
+
+                # Check subprocess's exit code (what to do with _run_plain_text() as it spawns multiple commands?)
+                # (It is not really necessary because we have an invalid XML then.)
+                # Does not quite conform dependency injection pattern:
+                new_xml_text = self.xml_run_command(self.state.opts.execution_context, script, self.interpreters, node, self.state.opts.command_runner).\
+                    run(self.state.xml_text)
+                if new_xml_text == self.state.xml_text:
+                    msg = self.state.opts.execution_context.translate("Iteration stopped to avoid infinite loop.")
+                    self.state.opts.error_logger.error(msg)
+                self.state.xml_text = new_xml_text
+                return
 
 
 class AssetsExhausted(StopIteration):
--- xmlboiler/core/alg/common.py
+++ xmlboiler/core/alg/common.py
@@ -16,41 +16,12 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import sys
-
-# it skips scripts for which there is no interpreter
-from xmlboiler.core.scripts.xml import Providers
-
-
-class RealNextScript(object):
-    def __init__(self, state, interpreters, xml_run_command=Providers.xml_run_command):
-        self.state = state
-        self.interpreters = interpreters
-        self.xml_run_command = xml_run_command
-
-    def step(self):
-        while True:
-            script = next(self.state.next_script)['script']
-            node = self.interpreters.find_interpreter(script.more.language, script.more.min_version,
-                                                      script.more.max_version)
-            if node is None:
-                self.state.failed_scripts.add(script)
-            else:
-                if script.more.script_url:
-                    self.state.opts.execution_context.logger.info(
-                        self.state.opts.execution_context.translations.gettext("Executed script {s}").format(s=script.more.script_url))
-                else:
-                    self.state.opts.execution_context.logger.info(
-                        self.state.opts.execution_context.translations.gettext("Executed script for {s}").format(s=script.more.language))
-                self.state.executed_scripts.add(script)
-
-                # Check subprocess's exit code (what to do with _run_plain_text() as it spawns multiple commands?)
-                # (It is not really necessary because we have an invalid XML then.)
-                # Does not quite conform dependency injection pattern:
-                new_xml_text = self.xml_run_command(self.state.opts.execution_context, script, self.interpreters, node, self.state.opts.command_runner).\
-                    run(self.state.xml_text)
-                if new_xml_text == self.state.xml_text:
-                    msg = self.state.opts.execution_context.translate("Iteration stopped to avoid infinite loop.")
-                    self.state.opts.error_logger.error(msg)
-                self.state.xml_text = new_xml_text
-                return
+def log_script_execution(state, script):
+    if script.more.script_url:
+        state.opts.execution_context.logger.info(
+            state.opts.execution_context.translations.gettext("Executed script {s}").format(
+                s=script.more.script_url))
+    else:
+        state.opts.execution_context.logger.info(
+            state.opts.execution_context.translations.gettext("Executed script for {s}").format(
+                s=script.more.language))
